1cea543df185aad7c03139c85749cae41fda21c2
AhiruNeko <AhiruNeko47@gmail.com>
2026-07-24 14:36:09 +0800

    add checkpoint config to bcrnn_transport

--- slurm_scripts/03_baselines/bcrnn_transport.sh
+++ slurm_scripts/03_baselines/bcrnn_transport.sh
@@ -12,4 +12,7 @@ mkdir -p logs
 
 python train.py --config-name=train_robomimic_lowdim_workspace.yaml \
     task=transport_lowdim training.seed=${SLURM_ARRAY_TASK_ID:-42} \
-    task.env_runner.n_envs=24 training.device=cuda:0
+    task.env_runner.n_envs=24 training.device=cuda:0 \
+    checkpoint.topk.k=1 checkpoint.topk.monitor_key=test_mean_score \
+    checkpoint.topk.mode=max checkpoint.save_last_ckpt=False \
+    task.env_runner.n_test_vis=1 task.env_runner.n_train_vis=0
